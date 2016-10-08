605ff2a47c9a8a21fbf78fcd783543927ad9ebd6
garry fan <fanjia@scutech.com>
2016-10-08 20:54:25 +0800

    Remove the compile objects

--- file_handler.d
+++
@@ -1,1 +0,0 @@
-file_handler.o file_handler.d : file_handler.cpp file_handler.h
--- main.d
+++
@@ -1,4 +0,0 @@
-main.o main.d : main.cpp download_manager.h download_manager_tmpl.h \
- http_curl_downloader_tmpl.h http_curl_downloader_tmpl.inl curl/curl.h \
- curl/curlver.h curl/curlbuild.h curl/curlrules.h curl/easy.h \
- curl/multi.h curl/curl.h download_manager_tmpl.inl file_handler.h
